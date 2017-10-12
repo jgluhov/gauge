4f5b9d7234c59bf3f86a21da62e04dab3f50e827
EVGENY GLUKHOV <jgluhov@gmail.com>
2017-10-12 20:35:34 +0300

    change configs

--- config/karma.ci.js
+++ config/karma.ci.js
@@ -4,6 +4,22 @@ const webpackMerge = require('webpack-merge'),
   module.exports = function(config) {
 
     const _config = webpackMerge(devConfig, {
+      files: [
+        'spec/main.ts',
+        'spec/**/*.spec.ts',
+        'spec/fixtures/*.fixture.html'
+      ],
+
+      preprocessors: {
+        'spec/main.ts': ['webpack'],
+        'src/**/*.ts': ['webpack'],
+        'spec/**/*.spec.ts': ['webpack'],
+        'spec/fixtures/*.fixture.html': ['html2js'],
+      },
+
+      reporters: ['progress'],
+
+      logLevel: config.LOG_INFO,
       singleRun: true
     });
 
